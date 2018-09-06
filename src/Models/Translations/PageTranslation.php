3442d8b94f12da562307baa91f0e588d8b9a0925
Dmitrijs Mihailovs <dmitrijs.mihailovs@netcore.lv>
2018-09-06 18:19:02 +0300

    Add sluggable to title

--- src/Models/Translations/PageTranslation.php
+++ src/Models/Translations/PageTranslation.php
@@ -2,10 +2,14 @@
 
 namespace Laradium\Laradium\Content\Models\Translations;
 
+use App\Models\Page;
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class PageTranslation extends Model
 {
+    use Sluggable;
 
     /**
      * @var array
@@ -23,4 +27,33 @@ class PageTranslation extends Model
 
         'locale'
     ];
+
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
+    /**
+     * Unique slug for each language
+     *
+     * @param Builder $query
+     * @param Model $model
+     * @param         $attribute
+     * @param         $config
+     * @param         $slug
+     */
+    public function scopeWithUniqueSlugConstraints(Builder $query, Model $model, $attribute, $config, $slug): void
+    {
+        $query->join('pages', 'page_translations.page_id', '=', 'pages.id')->where('locale', $this->locale);
+    }
 }
