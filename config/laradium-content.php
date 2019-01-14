0670cf4c748a79f4be8a83542701772465363105
speekadievs <arturs.lukjanenoks@netcore.lv>
2019-01-14 09:47:53 +0200

    Update readme and routes

--- config/laradium-content.php
+++ config/laradium-content.php
@@ -18,9 +18,16 @@ return [
     'use_homepage_slug' => false,
 
     'resolver' => [
+        // Defines the middlewares the resolver route will use
         'middlewares'    => ['web'],
+
+        // Change this to a function name, that will return the uri for the resolver route
         'custom_uri'     => null,
+
+        // Will add iso code before slug
         'prepend_locale' => false,
+
+        // Defines the controller and method, the resolver route will use
         'uses'           => '\Laradium\Laradium\Content\Http\Controllers\Admin\PageController@resolve'
     ]
 ];
--- routes/admin.php
+++ routes/admin.php
@@ -6,17 +6,9 @@ Route::group([
     'namespace'  => 'Admin',
     'middleware' => ['web', 'laradium'],
 ], function () {
-    Route::delete('content-block/{id}', [
-        'uses' => '\Laradium\Laradium\Content\Http\Controllers\Admin\PageController@contentBlockDelete'
-    ]);
+    Route::delete('content-block/{id}', '\Laradium\Laradium\Content\Http\Controllers\Admin\PageController@contentBlockDelete');
 
-    Route::get('pages/create/{channel}', [
-        'uses' => '\Laradium\Laradium\Content\Base\Resources\PageResource@create',
-        'as'   => 'pages.create'
-    ]);
+    Route::get('pages/create/{channel}', '\Laradium\Laradium\Content\Base\Resources\PageResource@create')->name('pages.create');
 
-    Route::get('pages/{page}/edit', [
-        'uses' => '\Laradium\Laradium\Content\Base\Resources\PageResource@edit',
-        'as'   => 'pages.edit'
-    ]);
+    Route::get('pages/{page}/edit', '\Laradium\Laradium\Content\Base\Resources\PageResource@edit')->name('pages.edit');
 });
